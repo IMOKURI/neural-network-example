fba6b5d48a8bb2ddd3f32ee805a18ac9afa5d39a
IMOKURI <nenegi.01mo@gmail.com>
2015-07-15 15:51:27 +0900

    Show trained network

--- src/Main.hs
+++ src/Main.hs
@@ -19,18 +19,29 @@ main = do
 
   let n = createNetwork normals g [l, l', l'']
 
-  let t = BackpropTrainer (3 :: Float) quadraticCost quadraticCost'
+  let t = BackpropTrainer 3 quadraticCost quadraticCost'
 
-  let dat = [(fromList [0, 1], fromList [1]), (fromList [1, 1], fromList [0]), (fromList [1, 0], fromList [1]), (fromList [0, 0], fromList [0])]
+  let dat = [(fromList [0, 1], fromList [1]),
+             (fromList [1, 1], fromList [0]),
+             (fromList [1, 0], fromList [1]),
+             (fromList [0, 0], fromList [0])]
 
   let n' = trainNTimes n t online dat 10000
 
   putStrLn "==> XOR predictions: "
+  putStr "(0,0): "
   print $ predict (fromList [0, 0]) n'
+  putStr "(1,0): "
   print $ predict (fromList [1, 0]) n'
+  putStr "(0,1): "
   print $ predict (fromList [0, 1]) n'
+  putStr "(1,1): "
   print $ predict (fromList [1, 1]) n'
 
+  putStrLn "\n==> Trained network: "
+  print $ map layerToShowable $ layers n'
+
+{-
   saveNetwork "xor.ann" n'
 
   putStrLn "==> Network saved and reloaded: "
@@ -40,4 +51,5 @@ main = do
   print $ predict (fromList [1, 0]) n''
   print $ predict (fromList [0, 1]) n''
   print $ predict (fromList [1, 1]) n''
+-}
 
